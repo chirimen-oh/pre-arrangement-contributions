7e3a96422b3fe772456d2a979e5f0e796a200f08
Satoru Takagi <satakagi@users.noreply.github.com>
2024-02-14 10:05:14 +0900

    Update main-GPS.js

--- GPS_PiZero/main-GPS.js
+++ GPS_PiZero/main-GPS.js
@@ -1,9 +1,17 @@
 // シリアルからGPSデータ受信
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
+import { requestI2CAccess } from "./node_modules/node-web-i2c/index.js";
+import SCD40 from "@chirimen/scd40";
+
+//const { SerialPort } = require('serialport')
+//const { ReadlineParser } = require('@serialport/parser-readline')
+//const { GPS } = require('gps');
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
+import GPSpackage from 'gps';
+const GPS = GPSpackage.GPS;
+
 const port = new SerialPort({ path: '/dev/ttyS0', baudRate: 9600 })
 const parser = port.pipe(new ReadlineParser())
-const { GPS } = require('gps');
 const gps = new GPS();
 
 parser.on('data', function (txtData) {
